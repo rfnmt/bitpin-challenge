0ec6d790c987ae99d82e652d0e425e8aca6bcd07
Erfan <erfan.nemati@digikala.com>
2024-04-11 17:05:56 +0330

    feat: add api service

---
+++ src/services/api.ts
@@ -0,0 +1,40 @@
+import axios, { AddressFamily } from "axios";
+import { GetAPIArgs } from "./types";
+
+// Note: Usually this should be in an ENV file
+const API_BASE_URL = "https://api.bitpin.org/";
+
+const TIMEOUT = 60000;
+
+const $axios = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: TIMEOUT,
+});
+
+$axios.interceptors.response.use((response) => {
+  return {
+    ...response,
+    data: response?.data,
+  };
+});
+
+export const serviceGet = <T>({
+  url,
+  queryParamsObj,
+  config,
+}: GetAPIArgs): Promise<T> => {
+  const finalUrl = `${url}?${new URLSearchParams(queryParamsObj || "")}`;
+
+  return new Promise((resolve, reject) => {
+    $axios
+      .get(finalUrl, config)
+      .then((response) => {
+        if (response?.status === 200) {
+          resolve(response.data);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
